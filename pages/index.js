f6d79d52f1d51d9948179b50b69efe341b690aea
Thomas <thomashabets99@gmail.com>
2020-02-13 14:14:06 +0100

    effecten staan er op, next up text

--- pages/index.js
+++ pages/index.js
@@ -8,122 +8,180 @@ import { withTranslation } from '../utils/i18n';
 const Home = ({ t }) => {
   console.log(t);
   return (
-    <Container>
-      <Deel1>
-        <WolkWrapper>
-          <Wolk
-            src="static/global/assets/images/onboarding/wolk2.png"
-            width="325"
-            height="236"
-            delay="-10s"
-            duration="150s"
-          />
-          <Wolk
-            src="static/global/assets/images/onboarding/wolk3.png"
-            width="359"
-            height="270"
-            delay="-180s"
-            duration="220s"
-          />
-          <Wolk
-            src="static/global/assets/images/onboarding/wolk4.png"
-            width="456"
-            height="375"
-            delay="-10s"
-            duration="220s"
-          />
-        </WolkWrapper>
-        <Whatever black>Hello Thomas</Whatever>
-        <WolkWrapper>
-          <Wolk
-            src="static/global/assets/images/onboarding/wolk1.png"
-            width="427"
-            height="287"
-            delay="-10s"
-            duration="220s"
-          />
-          <Wolk
-            src="static/global/assets/images/onboarding/wolk2.png"
-            width="325"
-            height="236"
-            delay="-120s"
-            duration="250s"
-          />
+    <ContainerWrapper>
+      <Container>
+        <Deel1>
+          <WolkWrapper>
+            <Wolk
+              src="static/global/assets/images/onboarding/wolk2.png"
+              width="325"
+              height="236"
+              delay="-10s"
+              duration="150s"
+            />
+            <Wolk
+              src="static/global/assets/images/onboarding/wolk3.png"
+              width="359"
+              height="270"
+              delay="-180s"
+              duration="220s"
+            />
+            <Wolk
+              src="static/global/assets/images/onboarding/wolk4.png"
+              width="456"
+              height="375"
+              delay="-10s"
+              duration="220s"
+            />
+          </WolkWrapper>
+          <Whatever>Hello Thomas</Whatever>
+          <WolkWrapper>
+            <Wolk
+              src="static/global/assets/images/onboarding/wolk1.png"
+              width="427"
+              height="287"
+              delay="-10s"
+              duration="220s"
+            />
+            <Wolk
+              src="static/global/assets/images/onboarding/wolk2.png"
+              width="325"
+              height="236"
+              delay="-120s"
+              duration="250s"
+            />
 
-          <Wolk
-            src="static/global/assets/images/onboarding/wolk5.png"
-            width="626"
-            height="422"
-            delay="-40s"
-            duration="200s"
-          />
-          <Wolk
-            src="static/global/assets/images/onboarding/wolk6.png"
-            width="259"
-            height="218"
-            delay="-100s"
-            duration="190s"
-          />
-        </WolkWrapper>
-      </Deel1>
-      <BushWrapper id="bushWrapper">
-        <BushLWrapper>
-          <Controller>
+            <Wolk
+              src="static/global/assets/images/onboarding/wolk5.png"
+              width="626"
+              height="422"
+              delay="-40s"
+              duration="200s"
+            />
+            <Wolk
+              src="static/global/assets/images/onboarding/wolk6.png"
+              width="259"
+              height="218"
+              delay="-100s"
+              duration="190s"
+            />
+          </WolkWrapper>
+        </Deel1>
+        <BushWrapper>
+          <Whatever>lorem ipsum alsabalanka testen</Whatever>
+          <BushLWrapper>
+            {/* <Controller>
             <Scene duration={600} classToggle="sweep" triggerElement=".leafLAnim" indicators>
               {(progress, event) => (
                 <div className="test">Pin Test {event.type} {progress}</div>
               )}
             </Scene>
-          </Controller>
-          <LeafLAnim duration=".85s">
-            <LeafL translateX="15" translateY="57" src="static/global/assets/images/onboarding/leaf11L.png" />
-          </LeafLAnim>
-          <LeafLAnim duration=".8s">
-            <LeafL translateX="30" translateY="47" src="static/global/assets/images/onboarding/leaf10L.png" />
-          </LeafLAnim>
-          <LeafLAnim duration=".75s">
-            <LeafL translateX="20" translateY="40" src="static/global/assets/images/onboarding/leaf9L.png" />
-          </LeafLAnim>
-          <LeafLAnim duration=".7s">
-            <LeafL translateX="5" translateY="50" src="static/global/assets/images/onboarding/leaf8L.png" />
-          </LeafLAnim>
-          <LeafLAnim duration=".65s">
-            <LeafL translateX="10" translateY="32" src="static/global/assets/images/onboarding/leaf7L.png" />
-          </LeafLAnim>
-          <LeafLAnim duration=".6s">
-            <LeafL translateX="15" translateY="35" src="static/global/assets/images/onboarding/leaf6L.png" />
-          </LeafLAnim>
-          <LeafLAnim duration=".55s">
-            <LeafL translateX="40" translateY="30" src="static/global/assets/images/onboarding/leaf5L.png" />
-          </LeafLAnim>
-          <LeafLAnim duration=".5s">
-            <LeafL translateX="5" translateY="28" src="static/global/assets/images/onboarding/leaf4L.png" />
-          </LeafLAnim>
-          <LeafLAnim duration=".45s">
-            <LeafL translateX="0" translateY="30" src="static/global/assets/images/onboarding/leaf3L.png" />
-          </LeafLAnim>
-          <LeafLAnim duration=".4s">
-            <LeafL translateX="0" translateY="15" src="static/global/assets/images/onboarding/leaf2L.png" />
-          </LeafLAnim>
-          <LeafLAnim duration=".45s">
-            <LeafL translateX="5" translateY="10" src="static/global/assets/images/onboarding/leaf1L.png" />
+          </Controller> */}
+            <Controller>
+              <Scene classToggle={['.test', 'sweep']} triggerElement="#leafTrigger" reverse={false} indicators>
+                <>
+                  <LeafLAnim translateX="15" translateY="57">
+                    <LeafL className="test" className="test" duration=".85s" src="static/global/assets/images/onboarding/leaf11L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="30" translateY="47">
+                    <LeafL className="test" duration=".8s" src="static/global/assets/images/onboarding/leaf10L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="20" translateY="40">
+                    <LeafL className="test" duration=".75s" src="static/global/assets/images/onboarding/leaf9L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="5" translateY="50">
+                    <LeafL className="test" duration=".7s" src="static/global/assets/images/onboarding/leaf8L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="10" translateY="32">
+                    <LeafL className="test" duration=".65s" src="static/global/assets/images/onboarding/leaf7L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="15" translateY="35">
+                    <LeafL className="test" duration=".6s" src="static/global/assets/images/onboarding/leaf6L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="40" translateY="30">
+                    <LeafL className="test" duration=".55s" src="static/global/assets/images/onboarding/leaf5L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="5" translateY="28">
+                    <LeafL className="test" duration=".5s" src="static/global/assets/images/onboarding/leaf4L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="0" translateY="30">
+                    <LeafL className="test" duration=".45s" src="static/global/assets/images/onboarding/leaf3L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="0" translateY="15">
+                    <LeafL className="test" duration=".4s" src="static/global/assets/images/onboarding/leaf2L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="5" translateY="10">
+                    <LeafL className="test" duration=".45s" src="static/global/assets/images/onboarding/leaf1L.png" />
+                  </LeafLAnim>
+                </>
+              </Scene>
+            </Controller>
+            <LeafTrigger id="leafTrigger" />
+          </BushLWrapper>
+          <BushRWrapper>
+            {/* <Controller>
+            <Scene duration={600} classToggle="sweep" triggerElement=".leafLAnim" indicators>
+              {(progress, event) => (
+                <div className="test">Pin Test {event.type} {progress}</div>
+              )}
+            </Scene>
+          </Controller> */}
+            <Controller>
+              <Scene classToggle={['.test', 'sweep']} triggerElement="#leafTrigger" reverse={false} indicators>
+                <>
+                  <LeafLAnim translateX="15" translateY="53">
+                    <LeafL className="test" duration=".85s" src="static/global/assets/images/onboarding/leaf11L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="33" translateY="47">
+                    <LeafL className="test" duration=".8s" src="static/global/assets/images/onboarding/leaf10L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="10" translateY="42">
+                    <LeafL className="test" duration=".75s" src="static/global/assets/images/onboarding/leaf9L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="3" translateY="53">
+                    <LeafL className="test" duration=".7s" src="static/global/assets/images/onboarding/leaf8L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="10" translateY="27">
+                    <LeafL className="test" duration=".65s" src="static/global/assets/images/onboarding/leaf7L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="15" translateY="35">
+                    <LeafL className="test" duration=".6s" src="static/global/assets/images/onboarding/leaf6L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="10" translateY="30">
+                    <LeafL className="test" duration=".55s" src="static/global/assets/images/onboarding/leaf5L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="5" translateY="28">
+                    <LeafL className="test" duration=".5s" src="static/global/assets/images/onboarding/leaf4L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="0" translateY="30">
+                    <LeafL className="test" duration=".45s" src="static/global/assets/images/onboarding/leaf3L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="0" translateY="15">
+                    <LeafL className="test" duration=".4s" src="static/global/assets/images/onboarding/leaf2L.png" />
+                  </LeafLAnim>
+                  <LeafLAnim translateX="5" translateY="10">
+                    <LeafL className="test" duration=".45s" src="static/global/assets/images/onboarding/leaf1L.png" />
+                  </LeafLAnim>
+                </>
+              </Scene>
+            </Controller>
+            <LeafTrigger id="leafTrigger" />
+          </BushRWrapper>
+        </BushWrapper>
+        <IceWrapperAchter>
+          <IceBackground zPos="0" src="static/global/assets/images/onboarding/achterkant-ice.png" />
+        </IceWrapperAchter>
+        <IceWrapperVoor>
+          <IceForground zPos="5" src="static/global/assets/images/onboarding/voorkant-ice.png" />
+        </IceWrapperVoor>
+        <CityWrapper>
+          <CityPeace src="static/global/assets/images/onboarding/huizen_links_voor.png" />
+          <CityPeace src="static/global/assets/images/onboarding/finish.png" />
+          <CityPeace src="static/global/assets/images/onboarding/huizen_rechts_voor.png" />
+        </CityWrapper>
+      </Container>
 
-          </LeafLAnim>
-
-
-        </BushLWrapper>
-        <BushR src="static/global/assets/images/onboarding/bush-right.png" />
-      </BushWrapper>
-      <IceWrapper>
-        <IceBackground zPos="0" src="static/global/assets/images/onboarding/achterkant-ice.png" />
-        <IceForground zPos="5" src="static/global/assets/images/onboarding/voorkant-ice.png" />
-      </IceWrapper>
-      {/* <CityWrapper>
-        <CityPeace src="static/global/assets/images/onboarding/huizen_links_voor.png" />
-        <CityPeace src="static/global/assets/images/onboarding/finish.png" />
-        <CityPeace src="static/global/assets/images/onboarding/huizen_rechts_voor.png" />
-      </CityWrapper> */}
-    </Container>
+    </ContainerWrapper>
   );
 };
 
@@ -139,12 +197,19 @@ const leafSweep = keyframes`
 
 const livingLeaf = keyframes`
  0% {transform: rotate(0);}
- 100% {transform: rotate(-1deg);}
+ 100% {transform: rotate(-3deg);}
+`;
+
+const LeafTrigger = styled.div`
+  transform: translateY(10rem);
 `;
 
 
 const Deel1 = styled.div`
-  height: 90vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
 `;
 
 const WolkWrapper = styled.div`
@@ -163,93 +228,104 @@ const BushWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
+  transform-origin: 100%;
+  transform: translateY(100vh) translateZ(4rem) scale(.5);
 `;
 
 const BushLWrapper = styled.div`
   margin-left: -15rem;
+  margin-top: 20rem;
+`;
+
+const BushRWrapper = styled.div`
+  margin-right: -15rem;
+  margin-top: -10rem;
+  transform: scaleX(-1);
 `;
 
 const LeafLAnim = styled.div`
-  
-  animation: ${livingLeaf} 4${(props) => props.duration} ease-in infinite alternate;
   position: absolute;
-
+  transform: translate(${(props) => props.translateX}rem, ${(props) => props.translateY}rem)};
+  position: absolute;
   .sweep{
-    animation: ${livingLeaf} 4${(props) => props.duration} ease-in infinite alternate, ${leafSweep} 0${(props) => props.duration} ease-out;
+    animation: ${leafSweep} 2${(props) => props.duration}s cubic-bezier(0.205, 0.450, 0.465, 1)  0${(props) => props.duration}s 1 alternate, ${livingLeaf} 4${(props) => props.duration}s ease-in  2${(props) => props.duration}s infinite alternate;
   }
 `;
 
 const LeafL = styled.img`
-  position: absolute;
-  
-  transform: translate(${(props) => props.translateX}rem, ${(props) => props.translateY}rem);
   transform-origin: top left;
+  transform: rotate(30deg);
 `;
 
-const BushR = styled.img`
-  display: block;
-`;
-
-const IceWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  overflow-x: hidden;
-  overflow-y: visible;
-  perspective: 8rem;
-  perspective-origin: 100%;
+const IceWrapperVoor = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  transform-origin: 100%;
+  transform: translateY(0) translateZ(4rem) scale(.5);  
+`;
+
+const IceWrapperAchter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform-origin: 100%;
+  transform: translateY(0) translateZ(3.5rem) scale(.5);
 `;
 
 const IceBackground = styled.img`
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  transform: translateZ(0);
+  top: 330vh;
 `;
 
 const IceForground = styled.img`
-  margin-top: auto;
-  margin-bottom: 50px;
-
-  transform-origin: 100%;
-  transform: translateZ(1rem) scale(0.9);
+  position: absolute;
+  top: 340vh;
 `;
 
 
 const CityPeace = styled.img`
-  margin: 0 -20rem;
 `;
 const CityWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: flex-end;
+  margin-top:250vh;
+  padding-top: 50vh;
 `;
 
 const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  perspective: 8rem;
+  perspective-origin: 100%;
+
   background: url(static/global/assets/images/onboarding/noise_V2.png),
-    linear-gradient(
-      184.99deg,
-      #cd5f16 1.87%,
-      #e19832 8.03%,
-      #f1da8b 13.87%,
-      #a7caed 22%,
-      #6c90b2 28.32%,
-      #3e5974 51.25%,
-      #192d3f 71.59%,
-      #091119 102.36%
-    );
-  background-attachment: fixed, scroll;
-  background-size: 6rem, auto;
-  min-height: 300vh;
-  max-width: 100vw;
-  margin: 0 auto;
+  linear-gradient(
+    184.99deg,
+    #192d3f 71.59%,
+    #091119 102.36%
+  );
 `;
 
+const ContainerWrapper = styled.div`
+  background: url(static/global/assets/images/onboarding/noise_V2.png),
+  linear-gradient(
+    #091119 102.36%
+  );
+  background-attachment: fixed, scroll;
+  background-size: 6rem, auto;
+  height: 100vh;
+
+`;
+
+
 const Whatever = styled.p`
-  color: white;
+  text-align: center;
+  position: absolute;
+  left: 50%
 `;
 
 // inser javascript
