9f1f921c0547678f05e1ac30bc0a834c50393ca0
Michaël Schauwers <michael@schauwe.rs>
2020-02-13 18:41:07 +0100

    ✨ Added image upload

--- components/Forms/CreateTeamForm/GenerateVideo.jsx
+++ components/Forms/CreateTeamForm/GenerateVideo.jsx
@@ -1,16 +1,25 @@
-import React, { useRef, useCallback } from 'react';
+import React, { createRef, useState } from 'react';
 import Webcam from 'react-webcam';
 import styled from 'styled-components';
 import { string } from 'prop-types';
+import loadFirebaseClient from '../../../utils/firebase';
+import 'firebase/firestore';
+import 'firebase/storage';
+import { useAuth } from '../../../hooks/useAuth';
+
 import FormLayout from '../../Layout/FormLayout';
 import Header from '../../Layout/Header';
-
-// import FormLayout from '../../Layout/FormLayout';
-// import Header from '../../Layout/Header';
+import Button from '../Inputs/Button';
 
 
 const GenerateVideo = ({ teamId }) => {
-  console.log('Team ID: ', teamId);
+  const [imageData, setImageData] = useState();
+
+  const { user } = useAuth();
+  const firebase = loadFirebaseClient;
+  const imageUploadRef = firebase.storage().ref(`images/${teamId}/${user.uid}/face.png`);
+  // const imageRef = storageRef.child('face.png');
+  // const imagesRef = storageRef.cild(`${teamId}/${user.uid}/face.png`);
 
   const videoConstraints = {
     width: 178,
@@ -18,11 +27,66 @@ const GenerateVideo = ({ teamId }) => {
     facingMode: 'user',
   };
 
-  const webcam = useRef(null);
+  const webcam = createRef();
+
+  const capture = () => {
+    setImageData(webcam.current.getScreenshot());
+  };
+
+  const resetCapture = () => {
+    setImageData();
+  };
+
+  const handleUploadImage = (e) => {
+    e.preventDefault();
+
+    // const image = new Image();
+    // image.src = imageData;
+
+    // console.log(image);
+
+    imageUploadRef.putString(imageData, 'data_url')
+      .then(() => {
+        console.log('Uploaded Image');
+      });
+
+    // firebase
+    //   .firestore()
+    //   .collection('teams')
+    //   .doc(teamId)
+    //   .add({
+    //     teamName: formData.teamName,
+    //     motivation: formData.motivation,
+    //     type: formData.type,
+    //     businessName: formData.businessName,
+    //     businessPhoto: formData.businessPhoto,
+    //     website: formData.website,
+    //     facebook: formData.website,
+    //     twitter: formData.twitter,
+    //     members: [
+    //       { user: user.uid },
+    //     ],
+    //     videos: [
+    //       { video: '' },
+    //       { video: '' },
+    //       { video: '' },
+    //       { video: '' },
+    //       { video: '' },
+    //     ],
+    //   })
+  };
+
+  const disableHandler = () => {
+    if (imageData) {
+      return false;
+    }
+    return true;
+  };
 
   // const capture = useCallback(
   //   () => {
   //     const imageSrc = webcam.current.getScreenshot();
+  //     console.log(imageSrc);
   //   },
   //   [webcam],
   // );
@@ -34,13 +98,29 @@ const GenerateVideo = ({ teamId }) => {
         text="Dit is jouw gepersonaliseerde promovideo, hier ga jij een hoofdrol spelen in het verhaal waarom jij meer teamgenoten nodig hebt. Selecteer een foto voor jouw karakter."
       />
       <CharacterWrapper>
-        <StyledWebcam
-          audio={false}
-          ref={webcam}
-          screenshotFormat="image/jpeg"
-          videoConstraints={videoConstraints}
-        />
+        { imageData ? <StyledPreview src={imageData} />
+          : (
+            <StyledWebcam
+              audio={false}
+              ref={webcam}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+            />
+          )}
       </CharacterWrapper>
+      <ButtonContainer>
+        { !imageData ? (
+          <>
+            <Button onClick={capture}>Capture photo</Button>
+            <Button onClick={capture}>Select a photo</Button>
+          </>
+        ) : (
+          <Button onClick={resetCapture}>Take new photo</Button>
+        )}
+      </ButtonContainer>
+      <SubmitWrapper>
+        <Button disabled={disableHandler()} onClick={(e) => handleUploadImage(e)}>Generate video</Button>
+      </SubmitWrapper>
     </FormLayout>
   );
 };
@@ -57,6 +137,29 @@ const StyledWebcam = styled(Webcam)`
   width: 17.8rem;
   height: 25.6rem;
   border-radius: 50%;
+`;
+
+const StyledPreview = styled.img`
+  width: 17.8rem;
+  height: 25.6rem;
+  border-radius: 50%;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & button {
+    margin-bottom: 0;
+  }
+`;
+
+const SubmitWrapper = styled.div`
+  margin-top: 2.8rem;
+  display: flex;
+  justify-content: flex-end;
 `;
 
 GenerateVideo.propTypes = {
